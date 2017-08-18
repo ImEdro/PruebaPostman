665cb8c67bec4daca2317d439fe807356745d30b
Carlos <Carlos@DESKTOP-QKM8J15>
2017-08-18 16:29:51 -0500

    Test

--- src/main/java/com/crunchify/jsp/servlet/HelloCrunchify.java
+++ src/main/java/com/crunchify/jsp/servlet/HelloCrunchify.java
@@ -1,5 +1,7 @@
 package com.crunchify.jsp.servlet;
  
+import DAO.PersonaJpaController;
+import VO.Persona;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * @author Crunchify.com
  */
@@ -17,6 +21,16 @@ public class HelloCrunchify extends HttpServlet {
         String surname=(String) request.getAttribute("surname");
         String phone=(String) request.getAttribute("phone");
         String mobile=(String) request.getAttribute("mobile");
-        
+        PersonaJpaController controller=new PersonaJpaController();
+        Persona p=new Persona();
+        p.setNombre(name);
+        p.setApellido(surname);
+        p.setTelefono(phone);
+        p.setCelular(mobile);
+        try {
+            controller.create(p);
+        } catch (Exception ex) {
+            Logger.getLogger(HelloCrunchify.class.getName()).log(Level.SEVERE, null, ex);
+        }
         }
 }
